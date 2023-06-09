364987d81cf78f9d61b3dccb4f76f2ab38dc087f
psferguson <petersferguson@gmail.com>
2023-06-09 11:22:02 -0700

    draft of class

--- python/lsst/sitcom/vandv/mount.py
+++ python/lsst/sitcom/vandv/mount.py
@@ -1,11 +1,22 @@
 import asyncio
+import logging
 import time
 import numpy as np
+import pandas as pd
 from scipy.signal import find_peaks
 from astropy.time import Time
+from lsst.sitcom.vandv.efd import create_efd_client
 
+__all__ = [
+    "move_mount_in_elevation_steps",
+    "slew_identifier",
+    "motion_state_slew_identifier"
+    
+]
 
-async def moveMountInElevationSteps(
+log = logging.getLogger(__name__)
+
+async def move_mount_in_elevation_steps(
     mount, target_el, azimuth=0, step_size=0.25, time_sleep=1
 ):
     """Move the mount from the current elevation angle to the target 
@@ -47,86 +58,6 @@ async def moveMountInElevationSteps(
         el = target_el
 
     return azimuth, el
-
-def get_slew_pairs(starts, stops, return_unmatched=False):
-    """
-    Given vectors of start times and stop times take the longer vector
-    and iterate over it. If that is `starts`, for each start time select all stop
-    times that are > than the start time and < than the next start time.
-    If multiple `stops` are detected select the minimum one. Also,
-    the unmatched start and stop times can be returned with `return_unmatched`.
-    Parameters
-    ----------
-    starts : float
-        slew start times
-    stops : float
-        slew stop times
-    return_unmatched : bool
-        Whether to return stops or starts that cannot be associated
-    
-    Returns
-    -------
-    starts : float
-        slew start times that match the returned stop times
-    stops : float
-        slew stop times that match the returned start times
-    """
-    new_starts = []
-    new_stops = []
-    unmatched_stops = []
-    unmatched_starts = []
-    
-    if (len(starts) ==0) | len(stops) == 0:
-        print("No slews identified")
-        return [], []
-
-    if len(stops) <= len(starts):
-        for i in range(len(starts)):
-            if i == len(starts) - 1:
-                stop_sel = stops > starts[i]
-            else:
-                stop_sel = (stops > starts[i]) & (stops < starts[i + 1])
-            if stop_sel.sum() == 1:
-                new_stops.append(stops[stop_sel][0])
-                new_starts.append(starts[i])
-            if stop_sel.sum() > 1:
-                new_stops.append(np.min(stops[stop_sel]))
-                new_starts.append(starts[i])
-                for j in stops[stop_sel]:
-                    if j != np.min(stops[stop_sel]):
-                        unmatched_stops.append(j)
-            if stop_sel.sum() == 0:
-                unmatched_starts.append(starts[i])
-
-    if len(stops) > len(starts):
-        for i in range(len(stops)):
-            if i == 0:
-                start_sel = (starts < stops[0]) & (starts > 0)
-            else:
-                start_sel = (starts < stops[i]) & (starts > stops[i - 1])
-            if start_sel.sum() == 1:
-                new_stops.append(stops[i])
-                new_starts.append(starts[start_sel][0])
-            if start_sel.sum() > 1:
-                new_stops.append(stops[i])
-                new_starts.append(np.max(starts[start_sel]))
-                for j in starts[start_sel]:
-                    if j != np.max(starts[start_sel]):
-                        unmatched_starts.append(j)
-            if start_sel.sum() == 0:
-                unmatched_stops.append(stops[i])
-    if (len(unmatched_starts) > 1) | (len(unmatched_stops) > 1):
-        print("unmatched stops or starts found")
-    if return_unmatched:
-        return (
-            np.array(new_starts),
-            np.array(new_stops),
-            unmatched_starts,
-            unmatched_stops,
-        )
-    else:
-        return np.array(new_starts), np.array(new_stops)
-
 
 def get_starts_command_track_target(
     command_track_target_times, command_track_target_positions, shift=0.1
@@ -314,3 +245,314 @@ def get_slew_from_mtmount(mt_mount_data_frame):
     mt_times = Time(mt_mount_data_frame["timestamp"], format="unix_tai").unix
     mt_velocities = mt_mount_data_frame["actualVelocity"]
     return get_slews_edge_detection_telemetry(mt_times, mt_velocities)
+
+
+class slew_identifier():
+    def __init__(self, method="motion_state"):
+        self.method=method
+    def get_slew_pairs(self, starts, stops, return_unmatched=False):
+        """
+        Given vectors of start times and stop times take the longer vector
+        and iterate over it. If that is `starts`, for each start time select all stop
+        times that are > than the start time and < than the next start time.
+        If multiple `stops` are detected select the minimum one. Also,
+        the unmatched start and stop times can be returned with `return_unmatched`.
+        Parameters
+        ----------
+        starts : float
+            slew start times
+        stops : float
+            slew stop times
+        return_unmatched : bool
+            Whether to return stops or starts that cannot be associated
+        
+        Returns
+        -------
+        starts : float
+            slew start times that match the returned stop times
+        stops : float
+            slew stop times that match the returned start times
+        """
+        new_starts = []
+        new_stops = []
+        unmatched_stops = []
+        unmatched_starts = []
+        
+        if (len(starts) ==0) | len(stops) == 0:
+            print("No slews identified")
+            return [], []
+
+        if len(stops) <= len(starts):
+            for i in range(len(starts)):
+                if i == len(starts) - 1:
+                    stop_sel = stops > starts[i]
+                else:
+                    stop_sel = (stops > starts[i]) & (stops < starts[i + 1])
+                if stop_sel.sum() == 1:
+                    new_stops.append(stops[stop_sel][0])
+                    new_starts.append(starts[i])
+                if stop_sel.sum() > 1:
+                    new_stops.append(np.min(stops[stop_sel]))
+                    new_starts.append(starts[i])
+                    for j in stops[stop_sel]:
+                        if j != np.min(stops[stop_sel]):
+                            unmatched_stops.append(j)
+                if stop_sel.sum() == 0:
+                    unmatched_starts.append(starts[i])
+
+        if len(stops) > len(starts):
+            for i in range(len(stops)):
+                if i == 0:
+                    start_sel = (starts < stops[0]) & (starts > 0)
+                else:
+                    start_sel = (starts < stops[i]) & (starts > stops[i - 1])
+                if start_sel.sum() == 1:
+                    new_stops.append(stops[i])
+                    new_starts.append(starts[start_sel][0])
+                if start_sel.sum() > 1:
+                    new_stops.append(stops[i])
+                    new_starts.append(np.max(starts[start_sel]))
+                    for j in starts[start_sel]:
+                        if j != np.max(starts[start_sel]):
+                            unmatched_starts.append(j)
+                if start_sel.sum() == 0:
+                    unmatched_stops.append(stops[i])
+        if (len(unmatched_starts) > 1) | (len(unmatched_stops) > 1):
+            print("unmatched stops or starts found")
+        if return_unmatched:
+            return (
+                np.array(new_starts),
+                np.array(new_stops),
+                unmatched_starts,
+                unmatched_stops,
+            )
+        else:
+            return np.array(new_starts), np.array(new_stops)
+
+    def combine_az_el(self, ):
+        # take identified slew starts and stops in az and el and combine to create master list of slew starts and stops
+        return "a"
+    
+class motion_state_slew_identifier(slew_identifier):
+    
+    def __init__(self, 
+                 start_time, 
+                 stop_time,
+                 axes = "both", 
+                 az_axis_motion_state = None, 
+                 az_in_position = None, 
+                 el_in_position = None, 
+                 el_axis_motion_state = None):
+        # datastream start and stop times in iso string format
+        self.start_time = start_time
+        self.stop_time = stop_time
+        self.axes = axes
+        self.efd_dict={}
+        self.efd_dict["az_axis_motion_state"] = az_axis_motion_state
+        self.efd_dict["az_in_position"] = az_in_position
+        self.efd_dict["el_axis_motion_state"] = el_axis_motion_state
+        self.efd_dict["el_in_position"] = el_in_position
+        
+        self.merged_timestamp_dict={}
+        self.slew_starts={}
+        self.slew_stops={}
+
+    async def get_data(self):
+        # if data is not passed query new data
+        if self.axes in ["azimuth", "az"]:
+            if self._check_if_query_needed(self.axes):
+                await self._query_efd(self.axes)
+        elif self.axes in ["elevation", "el"]:
+            if self._check_if_query_needed(self.axes):
+                await self._query_efd(self.axes)
+        elif self.axes == "both":
+            if self._check_if_query_needed(self.axes):
+                print("here")
+                await self._query_efd(self.axes)
+        else:
+            raise ValueError(f'axes must be one of: "both", "azimuth", "elevation", "az", "el" instead of {self.axes}')
+        
+        # check that there is data to identify slews with
+        for key in self.efd_dict.keys():
+            if self.efd_dict[key] is not None:
+                if self.efd_dict[key].empty:
+                    raise ValueError(f'{key} has no data, make sure there is data for the specified range')
+        
+    
+    def add_SlewState(self, axis_motion_state):
+        """
+        From conversation at:https://confluence.lsstcorp.org/display/LTS/23.06.05+Slew+Command+Sequences
+        
+        state= 0,1 : SlewState="stop"
+        state= 4,5 : SlewState="Tracking"
+        state= 2   : SlewState="movingPointToPoint"
+        state= 3   : what even is jogging
+        
+        """
+        if "snd_timestamp_utc" not in axis_motion_state.columns:
+            axis_motion_state["snd_timestamp_utc"]=self.convert_sndStamp_to_utc(
+                axis_motion_state["private_sndStamp"]
+                )
+        
+        axis_motion_state["SlewState"]="notSet"
+        
+        sel_stop=(axis_motion_state["state"]==0)  | (axis_motion_state["state"]==1)
+        axis_motion_state.loc[sel_stop,"SlewState"]="Stopped"
+        
+        sel_tracking=(axis_motion_state["state"]==4)  | (axis_motion_state["state"]==5)
+        axis_motion_state.loc[sel_tracking,"SlewState"]="Tracking"
+        
+        sel_point_to_point=(axis_motion_state["state"]==2)
+        axis_motion_state.loc[sel_point_to_point,"SlewState"]="movingPointToPoint"
+        
+        # check to make sure all SlewStates are set
+        
+        return axis_motion_state
+    
+    def run(self,):
+        
+        
+        
+        #ready dataframes
+        if self.axes in ["azimuth", "az", "both"]:
+            self.efd_dict["az_axis_motion_state"] = self.add_SlewState(self.efd_dict["az_axis_motion_state"])
+            if "snd_timestamp_utc" not in self.efd_dict["az_in_position"].columns:
+                self.efd_dict["az_in_position"]["snd_timestamp_utc"] = self.convert_sndStamp_to_utc(
+                    self.efd_dict["az_in_position"]["private_sndStamp"]
+                    )
+        if self.axes in ["elevation", "el", "both"]:
+            self.efd_dict["el_axis_motion_state"] = self.add_SlewState(self.efd_dict["el_axis_motion_state"])
+            if "snd_timestamp_utc" not in self.efd_dict["az_in_position"].columns:
+                self.efd_dict["el_in_position"]["snd_timestamp_utc"] = self.convert_sndStamp_to_utc(
+                    self.efd_dict["el_in_position"]["private_sndStamp"]
+                    )
+        
+        # identify slew starts and stops for each axis
+        if self.axes in ["azimuth", "az", "both"]:
+            self.get_slews(slew_id_axis="az")
+        if self.axes in ["elevation", "el", "both"]:
+            self.get_slews(slew_id_axis="el")
+        
+        
+    def convert_sndStamp_to_utc(self, private_sndStamp):
+        """given private_sndStamp data return values in utc"""
+        return Time(private_sndStamp, format="unix_tai").unix
+    
+    def get_slews(self, slew_id_axis):
+        if slew_id_axis not in ['az', 'el']:
+            raise ValueError(f"slew_id_axis must be 'az' or 'el' not: {slew_id_axis}")
+        slew_starts=[]
+        slew_stops=[]
+        current_axis_motion_state="NA"
+        previous_axis_motion_state="NA"
+        previous_in_position_state="NA"
+        current_in_position_state="NA"
+        previous_update="NA"
+        update="NA"
+        
+        # Merge dataframes to get all timestamps
+        self.merged_timestamp_dict[slew_id_axis]=pd.merge(
+            self.efd_dict[f"{slew_id_axis}_axis_motion_state"].loc[:,["snd_timestamp_utc","SlewState"]],
+            self.efd_dict[f"{slew_id_axis}_in_position"].loc[:,["snd_timestamp_utc","InPosition"]],
+            on="snd_timestamp_utc", 
+            how="outer", 
+            sort=True, 
+            indicator=True)
+        
+        for i,time in enumerate(self.merged_frame["snd_timestamp_utc"]):
+             # check if current state is tracking and previous slew state was stop or did not exist
+            previous_update=update
+            
+            if merged_frame["_merge"][i] == "left_only":
+                update="SlewState"
+                previous_axis_motion_state=current_axis_motion_state
+                current_axis_motion_state=merged_frame["SlewState"][i]
+            elif merged_frame["_merge"][i] == "right_only":
+                update="InPosition"
+                previous_in_position_state=current_in_position_state
+                current_in_position_state=merged_frame["InPosition"][i]
+
+            else:
+                update="both"
+                previous_axis_motion_state=current_axis_motion_state
+                current_axis_motion_state=merged_frame["SlewState"][i]
+                previous_in_position_state=current_in_position_state
+                current_in_position_state=merged_frame["InPosition"][i]
+                print("ahhhh")
+                #not sure about thisreturn
+                continue
+            if (update=="SlewState"):
+                if ((previous_axis_motion_state=="Stopped") | (previous_axis_motion_state=="NA")) & (current_axis_motion_state=="Tracking"): 
+                    # switch from stop to tracking is a slew start
+                    self.slew_starts[slew_id_axis].append(time)
+                if ((previous_axis_motion_state=="Stopped") & (current_axis_motion_state=="movingPointToPoint")):
+                    self.slew_starts[slew_id_axis].append(time)
+            if (update=="InPosition"):
+                if (current_axis_motion_state=="Tracking"):
+                    if (previous_in_position_state==True) & (current_in_position_state==False):
+                        # currently Tracking and switch from inPositon==True to False
+                        self.slew_starts[slew_id_axis].append(time)
+                    elif (previous_in_position_state==False) & (current_in_position_state==True):
+                        # currently Tracking and switch from inPositon==False to True
+                        self.slew_stops[slew_id_axis].append(time)
+                    elif (previous_update=="SlewState") & (previous_in_position_state=="NA") & (current_in_position_state==True):
+                        # catch the first slew stop in the dataframe"
+                        self.slew_stops[slew_id_axis].append(time)
+                    else: 
+                        print(f"no idea how we ended up here:{i,previous_axis_motion_state,current_axis_motion_state,previous_in_position_state, current_in_position_state}")
+                if (current_axis_motion_state=="Stopped") & (previous_axis_motion_state=="movingPointToPoint"):
+                    if (previous_in_position_state==False) & (current_in_position_state==True):
+                        # currently stopped after movePointToPoint and switch from inPositon==False to True
+                        self.slew_stops[slew_id_axis].append(time)
+            
+            
+    def _check_if_query_needed(self, axes):
+        check_list = []
+        if axes in ["azimuth", "az", "both"]:
+            check_list.append(self.efd_dict["az_axis_motion_state"] is None)
+            check_list.append(self.efd_dict["az_in_position"] is None)
+        elif axes in ["elevation", "el", "both"]:
+            check_list.append(self.efd_dict["el_axis_motion_state"] is None)
+            check_list.append(self.efd_dict["el_in_position"] is None)
+
+        if not any(item == False for item in check_list):
+            # no data is passed need to do query
+            return True
+        elif not any(item == True for item in check_list):
+            # all data is passed no need for query
+            return False
+        else:
+            raise ValueError("Must pass all dataframes or no dataframes")
+        
+    # async   def _query_efd(self, axes):
+    #             efd_names_dict={
+    #                 "az_pos":"lsst.sal.MTMount.logevent_azimuthInPosition",
+    #                 "el_pos":"lsst.sal.MTMount.logevent_elevationInPosition",
+    #                 "az_motion_state":"lsst.sal.MTMount.logevent_azimuthMotionState",
+    #                 "el_motion_state":"lsst.sal.MTMount.logevent_elevationMotionState",
+    #             }
+    #             client = create_efd_client()
+    #             if axes in ["azimuth", "az", "both"]:
+    #                 print("here")
+    #                 import pdb; pdb.set_trace()
+    #                 self.az_axis_motion_state= await client.select_time_series("lsst.sal.MTMount.logevent_azimuthMotionState", 
+    #                                                                         ["private_sndStamp", "state"], 
+    #                                                                         self.start_time, 
+    #                                                                         self.stop_time)
+    #                 self.az_in_position= await client.select_time_series("lsst.sal.MTMount.logevent_azimuthInPosition", 
+    #                                                                         ["private_sndStamp", "InPosition"], 
+    #                                                                         self.start_time, 
+    #                                                                         self.stop_time)
+    #             if axes in ["elevation", "el", "both"]:
+    #                 self.el_axis_motion_state= await client.select_time_series("lsst.sal.MTMount.logevent_elevationMotionState", 
+    #                                                                         ["private_sndStamp", "state"], 
+    #                                                                         self.start_time, 
+    #                                                                         self.stop_time)
+    #                 self.el_in_position= await client.select_time_series("lsst.sal.MTMount.logevent_elevationInPosition", 
+    #                                                                         ["private_sndStamp", "InPosition"], 
+    #                                                                         self.start_time, 
+    #                                                                         self.stop_time)
+    
+    
+    
+    
